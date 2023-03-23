9728ec50266173b5486e1faf748e4e48566a2daf
maudlst <74672202+maudlst@users.noreply.github.com>
2023-03-23 10:28:38 +0100

    commentaire #define

--- src/node.hpp
+++ src/node.hpp
@@ -1,10 +1,19 @@
 #include <string>
 #include <iostream>
-
+/**
+ * @brief  Le nombre de caractere qui represente une case dans une chaine de carractere
+ * 
+ */
 #define SQUARE_REPR_SIZE_IN_STRING 1
+/**
+ * @brief Le nombre de case sur une ligne dans le plateau
+ * 
+ */
 #define SIZE_SIDE 5
-#define CHAR_SEPARATOR_COL " "
-#define CHAR_SEPARATOR_LINE "\n"
+
+//#define CHAR_SEPARATOR_COL " "
+//#define CHAR_SEPARATOR_LINE "\n"
+
 /**
  * @brief Classe reprensentant un noeud du graphe
  * 
